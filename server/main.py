1b7d2c3631cde94e4cc3f6349adca9ff7a67df9e
Ifeoma Okoh <odibest1893@gmail.com>
2020-12-22 16:32:39 +0100

    Update main.py

--- server/main.py
+++ server/main.py
@@ -20,89 +20,117 @@ def index():
     return render_template("index.html")
 
 @app.route('/predict', methods=['GET', 'POST'])
-def predict():
-    upload = [request.files['file']]
-    for fp in upload:
-      ext = os.path.splitext(fp)[-1].lower()
-      if ext == ".png":
-    # if upload.endswith('.png'):
-        img = PILImage.create(upload)
-        # return f'{label} ({torch.max(probs).item()*100:.0f}%)'
-        score = learn.predict(img)
-        attentiveness_score = 0
-        for i in score[0]:
-          if i == 'bending':
-            attentiveness_score +=0.3
-          elif i == 'chatting':
-            attentiveness_score+=0.3
-          elif i == 'raising hand':
-            attentiveness_score+=0.9
-          elif i == 'sitting':
-            attentiveness_score+=0.5
-          elif i == 'standing':
-            attentiveness_score+=0.5
-          else:
-            attentiveness_score+=0.8
+# def predict():
+#     upload = [request.files['file']]
+#     for fp in upload:
+#       ext = os.path.splitext(fp)[-1].lower()
+#       if ext == ".png":
+#     # if upload.endswith('.png'):
+#         img = PILImage.create(upload)
+#         # return f'{label} ({torch.max(probs).item()*100:.0f}%)'
+#         score = learn.predict(img)
+#         attentiveness_score = 0
+#         for i in score[0]:
+#           if i == 'bending':
+#             attentiveness_score +=0.3
+#           elif i == 'chatting':
+#             attentiveness_score+=0.3
+#           elif i == 'raising hand':
+#             attentiveness_score+=0.9
+#           elif i == 'sitting':
+#             attentiveness_score+=0.5
+#           elif i == 'standing':
+#             attentiveness_score+=0.5
+#           else:
+#             attentiveness_score+=0.8
         
-        avgScore = attentiveness_score/len(score[0])
+#         avgScore = attentiveness_score/len(score[0])
         
-        return f"Predictions: {score[0]}\nThe Attentiveness Score for this Frame is: {avgScore:.01f}"
+#         return f"Predictions: {score[0]}\nThe Attentiveness Score for this Frame is: {avgScore:.01f}"
 
-      elif ext == ".mp4":
-          for fn in upload.keys():
-            vidcap = cv2.VideoCapture(fn)
-          def getFrame(sec):
-              vidcap.set(cv2.CAP_PROP_POS_MSEC,sec*1000)
-              hasFrames,image = vidcap.read()
-              if hasFrames:
-                  cv2.imwrite("image"+str(count)+".png", image)     # save frame as JPG file
-              return hasFrames
-          sec = 0
-          frameRate = 1 #//it will capture image in each 0.5 second
-          count=1
-          success = getFrame(sec)
-          while success:
-              count = count + 5
-              sec = sec + frameRate
-              sec = round(sec, 2)
-              success = getFrame(sec)
+#       elif ext == ".mp4":
+#           for fn in upload.keys():
+#             vidcap = cv2.VideoCapture(fn)
+#           def getFrame(sec):
+#               vidcap.set(cv2.CAP_PROP_POS_MSEC,sec*1000)
+#               hasFrames,image = vidcap.read()
+#               if hasFrames:
+#                   cv2.imwrite("image"+str(count)+".png", image)     # save frame as JPG file
+#               return hasFrames
+#           sec = 0
+#           frameRate = 1 #//it will capture image in each 0.5 second
+#           count=1
+#           success = getFrame(sec)
+#           while success:
+#               count = count + 5
+#               sec = sec + frameRate
+#               sec = round(sec, 2)
+#               success = getFrame(sec)
 
-          # score = learn_inf.predict(img)
-          global df
-          dataframe = []
-          for frame in glob.iglob('*.png'):
-            # print(learn_inf.predict(frame))
-            value = learn_inf.predict(frame)
-            # print(value[0])
+#           # score = learn_inf.predict(img)
+#           global df
+#           dataframe = []
+#           for frame in glob.iglob('*.png'):
+#             # print(learn_inf.predict(frame))
+#             value = learn_inf.predict(frame)
+#             # print(value[0])
 
 
-            attentiveness_score = 0
-            for i in value[0]:
-              if i == 'bending':
-                attentiveness_score +=0.3
-              elif i == 'chatting':
-                attentiveness_score+=0.3
-              elif i == 'raising hand':
-                attentiveness_score+=0.9
-              elif i == 'sitting':
-                attentiveness_score+=0.5
-              elif i == 'standing':
-                attentiveness_score+=0.5
-              else:
-                attentiveness_score+=0.8
-            try:
-              avgScore = round(attentiveness_score/len(value[0]), 1)
-            except:
-              pass
-            dataframe.append([frame, value[0], avgScore])
+#             attentiveness_score = 0
+#             for i in value[0]:
+#               if i == 'bending':
+#                 attentiveness_score +=0.3
+#               elif i == 'chatting':
+#                 attentiveness_score+=0.3
+#               elif i == 'raising hand':
+#                 attentiveness_score+=0.9
+#               elif i == 'sitting':
+#                 attentiveness_score+=0.5
+#               elif i == 'standing':
+#                 attentiveness_score+=0.5
+#               else:
+#                 attentiveness_score+=0.8
+#             try:
+#               avgScore = round(attentiveness_score/len(value[0]), 1)
+#             except:
+#               pass
+#             dataframe.append([frame, value[0], avgScore])
 
-          pd.set_option('display.max_colwidth', -1)
-          df = pd.DataFrame(dataframe, columns=["frame", "predicted_classes", "attentiveness_score"])
-          # lbl_pred.value = f'{pd.DataFrame(dataframe, columns=["frame", "predicted_classes", "attentiveness_score"])}'
-          print(df)
+#           pd.set_option('display.max_colwidth', -1)
+#           df = pd.DataFrame(dataframe, columns=["frame", "predicted_classes", "attentiveness_score"])
+#           # lbl_pred.value = f'{pd.DataFrame(dataframe, columns=["frame", "predicted_classes", "attentiveness_score"])}'
+#           print(df)
       
+#       else:
+#         print('Upload a video or an Image')
+
+def predict():
+    img = PILImage.create(request.files['file'])
+    # label,_,probs = learn.predict(img)
+    # return f'{label} ({torch.max(probs).item()*100:.0f}%)'
+    # lbl_pred.value = f'Prediction: {pred}; Probability: {probs[pred_idx]:.04f}'
+    # img = PILImage.create(btn_upload.data[-1])
+    # out_pl.clear_output()
+    # with out_pl: display(img.to_thumb(128,128))
+    score = learn.predict(img)
+    attentiveness_score = 0
+    for i in score[0]:
+      if i == 'bending':
+        attentiveness_score +=0.3
+      elif i == 'chatting':
+        attentiveness_score+=0.3
+      elif i == 'raising hand':
+        attentiveness_score+=0.9
+      elif i == 'sitting':
+        attentiveness_score+=0.5
+      elif i == 'standing':
+        attentiveness_score+=0.5
       else:
-        print('Upload a video or an Image')
+        attentiveness_score+=0.8
+      
+    avgScore = attentiveness_score/len(score[0])
+    
+    return f"Predictions: {score[0]}\nThe Attentiveness Score for this Frame is: {avgScore:.01f}"
 
 
 if __name__=='__main__':
